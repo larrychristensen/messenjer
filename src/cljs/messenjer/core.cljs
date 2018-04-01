91d28385baeab53ab41854ed3c89b6bac06c6c3f
Larry <larry.d.christensen@gmail.com>
2018-04-01 13:01:29 -0600

    work backwards to part-1

--- src/cljs/messenjer/core.cljs
+++ src/cljs/messenjer/core.cljs
@@ -1,11 +1,25 @@
 (ns messenjer.core
-  (:require [reagent.core :as reagent]
-            [messenjer.views.messajes :as mv]
-            [messenjer.subs.messajes]
-            [messenjer.events.messajes]
-            [re-frame.core :refer [dispatch-sync]]))
+    (:require [reagent.core :as reagent :refer [atom]]))
 
 (enable-console-print!)
 
-(reagent/render-component [mv/messajes-page]
+(println "This text is printed from src/messenjer/core.cljs. Go ahead and edit it and see reloading in action.")
+
+;; define your app data so that it doesn't get over-written on reload
+
+(defonce app-state (atom {:text "Hello world!"}))
+
+
+(defn hello-world []
+  [:div
+   [:h1 (:text @app-state)]
+   [:h3 "I edited this, did it change?"]])
+
+(reagent/render-component [hello-world]
                           (. js/document (getElementById "app")))
+
+(defn on-js-reload []
+  ;; optionally touch your app-state to force rerendering depending on
+  ;; your application
+  ;; (swap! app-state update-in [:__figwheel_counter] inc)
+)
