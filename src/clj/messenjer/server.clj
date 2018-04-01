a015dcd973e378541f765e9b1b73e7b5fed0aab4
Larry <larry.d.christensen@gmail.com>
2018-04-01 12:52:59 -0600

    work back to step 2

--- src/clj/messenjer/server.clj
+++
@@ -1,7 +0,0 @@
-(ns messenjer.server
-  (:require [messenjer.system :as s]
-            [com.stuartsierra.component :as component])
-  (:gen-class))
-
-(defn -main []
-  (component/start (s/system :prod)))
