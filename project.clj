5f7f5c4413b6e8480e954f2d71af692f0f7c5661
Larry Christensen <user@sofi.org>
2018-04-19 09:25:53 -0600

    Fix project.clj

--- project.clj
+++ project.clj
@@ -27,7 +27,7 @@
             [lein-garden "0.3.0"]
             [lein-cljsbuild "1.1.7" :exclusions [[org.clojure/clojure]]]]
 
-  :source-paths ["src/clj" "src/cljc" "src/cljs"]
+  :source-paths ["src/clj" "src/cljs"]
 
   :garden {:builds [{:source-paths ["src/clj"]
                      :stylesheet messenjer.styles.core/app
